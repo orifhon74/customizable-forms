55bfea9be0d83140edb70384d9d7f417452af2ca
orifhonkilichev <orifhon017@gmail.com>
2024-12-01 21:37:17 +0500

    env fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return (
-      <div>
-        <h1>Hello, World</h1>
-      </div>
-  );
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}/`)
+            .then((response) => response.text())
+            .then((data) => setMessage(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    );
 }
 
 export default App;
