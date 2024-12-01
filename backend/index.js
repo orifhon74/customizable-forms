cd0dd93fa8340a7a145fdd208d2997185e34dcff
orifhonkilichev <orifhon017@gmail.com>
2024-12-01 21:56:28 +0500

    update for deployment

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, World!' });
 });
 
 const PORT = process.env.PORT || 5001;
--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}/`)
-            .then((response) => response.text())
-            .then((data) => setMessage(data));
+        fetch('/api/hello') // No need for the full URL when using proxy
+            .then((response) => response.json())
+            .then((data) => setMessage(data.message))
+            .catch((error) => console.error('Error fetching API:', error));
     }, []);
 
     return (
