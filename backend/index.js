7eb86ab56e0095623f1979b6c10bb376468ed18c
orifhonkilichev <orifhon017@gmail.com>
2024-12-01 21:45:51 +0500

    update package 2

--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
