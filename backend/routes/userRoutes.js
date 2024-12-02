69f5d9347cd3fb82a963751a49d0c11ebc1a0d71
orifhonkilichev <orifhon017@gmail.com>
2024-12-03 01:07:35 +0500

    bcrypt change to bcryptjs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User'); // Sequelize User model
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Create a new user
 router.post('/', async (req, res) => {
